3086c755a4aaff8273cabbe7ae0de12a66093415
Karthikeya V <bikted29@gmail.com>
2025-12-18 10:25:50 +0530

    Implement singleNumber method using HashMap

--- 136 leetcode.java
+++ 136 leetcode.java
@@ -7,3 +7,19 @@ class Solution {
         return s;
     }
 }
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int s:nums){
+           hm.put(s,hm.getOrDefault(s,0)+1); 
+        }
+        for(int s:nums){
+            if(hm.get(s)==1){
+                return s;
+            }
+        }
+        return -1;
+    }
+}
